05a2b411a632a9f3f3780d85db872ea5316ce85a
g-saldanha <saldanha@bridge.ufsc.br>
2017-03-27 11:51:21 -0300

    Add files via upload

--- src/entidades/Amigo.java
+++ src/entidades/Amigo.java
@@ -2,4 +2,40 @@ package entidades;
 
 public class Amigo {
 
+	private Pessoa nome;
+
+	private int telefone;
+
+	private String email;
+	
+	public Amigo(Pessoa nome, int telefone, String email) {
+		this.nome = nome;
+		this.telefone = telefone;
+		this.email = email;
+	}
+	
+	public Pessoa getNome() {
+		return nome;
+	}
+
+	public void setNome(Pessoa nome) {
+		this.nome = nome;
+	}
+
+	public int getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 }
---
+++ src/entidades/Livro.java
@@ -0,0 +1,48 @@
+package entidades;
+
+public class Livro {
+
+	private String titulo;
+
+	private String resumo;
+
+	private Pessoa autor;
+
+	private Pessoa personagemPrincipal;
+
+	private String genero;
+
+	private String faixaEtaria;
+
+	private boolean estaLocado;
+
+	private Pessoa quemLocou;
+	
+	public Livro(Pessoa autor, String resumo, String titulo, Pessoa personagemPrincipal, String genero, String faixaEtaria) {
+		this.autor = autor;
+		this.resumo = resumo;
+		this.titulo = titulo;
+		this.personagemPrincipal = personagemPrincipal;
+		this.genero = genero;
+		this.faixaEtaria = faixaEtaria;
+		this.estaLocado = false;
+		this.quemLocou = null;
+	}
+
+	public boolean isEstaLocado() {
+		return estaLocado;
+	}
+
+	public void setEstaLocado(boolean estaLocado) {
+		this.estaLocado = estaLocado;
+	}
+
+	public Pessoa getQuemLocou() {
+		return quemLocou;
+	}
+
+	public void setQuemLocou(Pessoa quemLocou) {
+		this.quemLocou = quemLocou;
+	}
+
+}
